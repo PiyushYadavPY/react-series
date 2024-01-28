95441ad23c8ab315a62e46529917715df1ec4fa6
piyushyadavpy <piyushyadav4991@gmail.com>
2024-01-28 12:25:33 +0530

    react router optimized using loader

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import { Route, RouterProvider, Routes, createBrowserRouter, createRoutesFromElements} from 'react-router-dom'
 import Layout from './Layout.jsx'
@@ -11,48 +10,49 @@ import User from './components/User/User.jsx'
 import Github, {githubInfoLoader} from './components/Github/Github.jsx'
 
 
-// const router = createBrowserRouter([
-//   {
-//     path: '/',
-//     element: <Layout/>,
-//     children:[
-//       {
-//         path: "",
-//         element:<Home />
-//       },
-//       {
-//         path:"about",
-//         element: <About />
-//       },
-//       {
-//         path:"contact",
-//         element: <Contact />
-//       },
-//       {
-//         path:"user/:userId",
-//         element: <User/>
-//       },
-//       {
-//         path:"/github",
-//         element: <Github />
-//       }
-//     ]
-//   }
-// ])
 const router = createBrowserRouter([
-  createRoutesFromElements(
-    <Route path='/' element={<Layout />}>
-    <Route path='' element={<Home />} />
-    <Route path='about' element={<About />} />
-    <Route path='contact' element={<Contact />} />
-    <Route path='user/:userid' element={<User />} />
-    <Route 
-    loader={githubInfoLoader}
-    path='github' 
-    element={<Github />}
-     />
-  </Route>  )
-]);
+  {
+    path: '/',
+    element: <Layout/>,
+    children:[
+      {
+        path: "",
+        element:<Home />
+      },
+      {
+        path:"about",
+        element: <About />
+      },
+      {
+        path:"contact",
+        element: <Contact />
+      },
+      {
+        path:"user/:userId",
+        element: <User/>
+      },
+      {
+        path:"/github",
+        element: <Github />,
+        loader: githubInfoLoader
+      }
+    ]
+  }
+])
+// const router = createBrowserRouter([
+//   createRoutesFromElements(
+//     <Route path='/' element={<Layout />}>
+//     <Route path='' element={<Home />} />
+//     <Route path='about' element={<About />} />
+//     <Route path='contact' element={<Contact />} />
+//     <Route path='user/:userid' element={<User />} />
+//     <Route 
+//     loader={githubInfoLoader}
+//     path='github' 
+//     element={<Github />}
+//      />
+//   </Route>  )
+// ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
