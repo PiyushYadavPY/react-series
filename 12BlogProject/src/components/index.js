4027ee28e2f829d9887f97d6cea9ae124288edbe
piyushyadavpy <piyushyadav4991@gmail.com>
2024-04-14 20:33:07 +0530

    blog done

---
+++ 12BlogProject/src/components/index.js
@@ -0,0 +1,30 @@
+import Header from "./Header/header";
+import Footer from "./Footer/footer";
+import Logo from "./Logo";
+import Container from "./container/Container";
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import PostCard from "./PostCard";
+import PostForm from "./post-form/PostForm";
+import AuthLayout from "./AuthLayout";
+import Input from "./Input";
+import Select from "./Select";
+import Button from "./Button";
+export {
+  Header,
+  Footer,
+  Logo,
+  Container,
+  LogoutBtn,
+  RTE,
+  Signup,
+  Login,
+  PostCard,
+  PostForm,
+  AuthLayout,
+  Input,
+  Select,
+  Button,
+};
